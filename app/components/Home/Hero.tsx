4b4ae20788ec77ff8bddd55b3d5015a92ef6a986
Jordan Higuera <jordanwhiguera@gmail.com>
2024-02-04 17:15:07 -0500

    about us section and image now next to each other

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -4,31 +4,36 @@ import Container from "../Container";
 
 const Hero: React.FC = () => {
   return (
-    <Container className="flex flex-1">
+    <Container>
       {/* Text Section */}
-      <div className="flex flex-col justify-center py-8 pr-8 md:w-1/2">
+      <div className="py-8 md:py-16">
         <h1 className="text-5xl font-bold mb-4">CAPTURING</h1>
         <p className="text-2xl mb-4">THE MOMENTS THAT CAPTIVATE YOUR HEART</p>
-        <div className="bg-black text-white p-4">
+      </div>
+
+      {/* About Us & Image Section */}
+      <div className="flex flex-col md:flex-row items-stretch md:space-x-4">
+        {/* About Us Section */}
+        <div className="bg-black text-white p-4 flex flex-col justify-between md:w-1/3">
           <p className="mb-4">
             Amelia Allen is a lifestyle, portrait and documentary photographer
             from Somerset who now lives and works in London.
           </p>
-          <button className="flex items-center">
+          <button className="flex items-center mt-4">
             <span className="font-semibold mr-2">ABOUT US</span>
             <span className="text-xl">↑</span>{" "}
             {/* Replace with an actual arrow icon */}
           </button>
         </div>
-      </div>
 
-      {/* Image Section */}
-      <div className="md:w-1/2 flex justify-end">
-        <img
-          src="/images/stowbasepreview.png" // Replace with the actual path to your image
-          alt="Photographer"
-          className="w-full h-full object-cover"
-        />
+        {/* Image Section */}
+        <div className="md:w-2/3">
+          <img
+            src="/images/stowbasepreview.png" // Replace with the actual path to your image
+            alt="Photographer"
+            className="w-full h-full object-cover"
+          />
+        </div>
       </div>
     </Container>
   );
