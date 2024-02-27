6afa12a552baa6bcae9733c0086b80e65f0b5d0b
Jordan Higuera <jordanwhiguera@gmail.com>
2024-02-26 23:28:36 -0500

    images load fater

--- app/components/Projects/PhotographyShowcase.tsx
+++ app/components/Projects/PhotographyShowcase.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
-import Container from "../Container"; // Ensure this is the correct path to your Container component
+import Container from "../Container";
+import Image from "next/image";
 
 const PhotographyShowcase: React.FC = () => {
   return (
@@ -9,97 +10,133 @@ const PhotographyShowcase: React.FC = () => {
         <h2 className="text-3xl lg:text-5xl font-bold mb-8">PHOTOS</h2>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {/* Replace the src attribute with the actual paths to your images */}
-          <img
+          <Image
             src="/images/IG-7.webp"
             alt="Home exterior"
-            className="w-full h-full  rounded-3xl"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/IG-6.webp"
-            alt="Living room"
-            className="w-full h-full  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/IG-3.webp"
-            alt="Kitchen"
-            className="w-full h-full  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/IG-5.webp"
-            alt="Kitchen"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/IG-1.webp"
-            alt="Backyard pool"
-            className="w-full h-full r rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
 
-          <img
+          <Image
             src="/images/IG-2.webp"
-            alt="Sunset view"
-            className="w-full h-auto rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
 
-          <img
+          <Image
             src="/images/Lofgren-HD-2.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Lofgren-HD-3.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Lofgren-HD-6.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Lofgren-HD-7.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Lofgren-HD-8.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Deville-HD-2.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Deville-HD-3.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Deville-HD-4.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Deville-HD-5.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Deville-HD-6.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Deville-HD-9.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
-          <img
+          <Image
             src="/images/Deville-HD-8.webp"
-            alt="Sunset view"
-            className="w-full h-auto  rounded-3xl"
+            alt="Home exterior"
+            width={500} // Set appropriate width
+            height={100} // Set appropriate height
+            className="rounded-3xl w-full h-full"
           />
         </div>
       </div>
