ba36f8cfa4d38724ced939dad49f3388ddf09bb3
Jordan Higuera <jordanwhiguera@gmail.com>
2025-01-15 22:36:18 -0500

    remove vid

--- app/components/Projects/PhotographyShowcase.tsx
+++ app/components/Projects/PhotographyShowcase.tsx
@@ -117,36 +117,6 @@ const PhotographyShowcase: React.FC = () => {
             className="rounded-3xl w-full"
           />
         </div>
-
-        {/* Videos Section */}
-        <h3 className="text-3xl lg:text-5xl font-bold mt-12 mb-8">VIDEOS</h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <div className="relative group">
-            <video
-              ref={videoRef}
-              className="rounded-3xl w-full"
-              poster="/images/Drone.webp"
-              controls
-              onPlay={handleVideoState}
-              onPause={handleVideoState}
-            >
-              <source src="/videos/Drone Vid v1.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-3xl opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none">
-              <div
-                className="flex items-center justify-center w-16 h-16 bg-white text-black rounded-full text-2xl font-bold group-hover:scale-110 transform transition-transform duration-300 pointer-events-auto"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handlePlayPause();
-                }}
-              >
-                {isPlaying ? "||" : "▶"}
-              </div>
-            </div>
-          </div>
-          {/* Add more videos if needed */}
-        </div>
       </div>
     </Container>
   );
