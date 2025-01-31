3b134077860d01d519ff77c8eb37b102a965fc93
Jordan Higuera <jordanwhiguera@gmail.com>
2025-01-31 01:37:37 -0500

    new pic

--- app/components/Home/PhotoSection.tsx
+++ app/components/Home/PhotoSection.tsx
@@ -19,7 +19,7 @@ const PhotoSection: React.FC = () => {
         </div>
         <div className="w-full">
           <Image
-            src="/images/cro ex new-1.webp"
+            src="/images/cro ex-8.webp"
             //  src="/images/mar conc n-1.webp"
             alt="Second"
             width={400} // Set appropriate width
