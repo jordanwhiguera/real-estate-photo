9d0b5aa5f78f138f4c0cc80d6a143bbf984d6a96
Jordan Higuera <jordanwhiguera@gmail.com>
2025-08-14 19:03:09 -0400

    new g tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-S2HDN0SPD2"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11475977860"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -27,7 +27,7 @@ export default function RootLayout({
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-S2HDN0SPD2');
+          gtag('config', 'AW-11475977860');
         `}
       </Script>
       <body className={inter.className}>{children}</body>
